3e8149b36f35c4b626b71ebe340d056740fd2a67
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-12 01:09:21 +0600

    sample.rs file added

---
+++ sample.rs
@@ -0,0 +1,11 @@
+fn main () {
+    let number = 10;
+    
+    if number < 10 {
+        println!("the number is less than 10")
+    } else if number == 10 {
+        println!("the number is exactly 10")
+    } else {
+        println!("the number is greater than 10")
+    }
+}
